12d96c863acee370c7e46b0d4e0e5e822e22d270
KrasenCheneshkov <krasen.cheneshkov@gmail.com>
2024-09-17 16:32:58 +0300

    Create right_angled_triangle_numbers.py

---
+++ right_angled_triangle_numbers.py
@@ -0,0 +1,10 @@
+# Write a Python program that prints a right-angled triangle using numbers. The user will provide the number of rows.
+
+rows = int(input())
+
+for i in range(1, rows + 1):
+    for j in range(1, rows + 1):
+        print(j, end='')
+        if j == i:
+            break
+    print()
